d8f4f579362fb282188b2d4e81e7fdbf043fb8fb
vijeshnk <vijeshnirala@gmail.com>
2022-12-29 14:45:37 +0530

    redux toolkit tailwind css setup

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="container mx-auto bg-red-500">
+        <h6>Navbar</h6>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
